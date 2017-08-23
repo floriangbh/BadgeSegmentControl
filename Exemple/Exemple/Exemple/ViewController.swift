ea33db3d065ce848b16e85b3965a9708511c4b07
Florian Gabach <florian.gabach@gmail.com>
2017-08-23 21:44:53 +0200

    Add exemple / test project

---
+++ Exemple/Exemple/Exemple/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Exemple
+//
+//  Created by Florian Gabach on 22/08/2017.
+//  Copyright © 2017 Florian Gabach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
